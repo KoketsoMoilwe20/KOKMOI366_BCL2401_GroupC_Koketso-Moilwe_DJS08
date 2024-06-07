568841a614426e458fc8ccb2afb31b611f6ae145
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-06-07 19:48:23 +0200

    Wrap Income and Reviews inside Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -39,11 +39,13 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
         <Route path="/vans/:id" element={<VanDetail />} />
-        <Route path="/host" element={<Dashboard />} />
-        <Route path="/host/income" element={<Income />} />
-        <Route path="/host/reviews" element={<Reviews />} />
+        
+        <Route path="/host" element={<Dashboard />}>
+          <Route path="/host/income" element={<Income />} />
+          <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
         </Route>
-      
+
     </Routes>
     </BrowserRouter>
   )
