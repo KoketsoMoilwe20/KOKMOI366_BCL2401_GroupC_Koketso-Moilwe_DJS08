1197c7ff6e1c54b1740af6145691faf259c2a765
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-06-07 15:33:17 +0200

    Cut header element

--- src/App.jsx
+++ src/App.jsx
@@ -27,13 +27,7 @@ function App() {
 
   return (
     <BrowserRouter>
-    <header>
-      <Link to="/">#VANLIFE</Link>
-      <nav>
-        <Link to="/about">About</Link>
-        <Link to="/vans">Vans</Link>
-      </nav>
-    </header>
+    
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
