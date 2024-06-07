83b857da6062a041636f01cd75ee15c08f386483
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-06-07 10:44:14 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,26 @@
  * linked in the slides.
  */
 
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home"
+import About from "./pages/About"
+
 function App() {
 
   return (
-    <h1>Start here</h1>
+    <BrowserRouter>
+    <header>
+      <Link to="/">#VANLIFE</Link>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </header>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
