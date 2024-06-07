cf511de7a1ee2d423bb49031a1f368324c27428b
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-06-07 13:50:26 +0200

    Import useParams hook
    And create the params object using the hook

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -1,5 +1,8 @@
 import React from "react"
+import { useParams } from "react-router-dom"
 
 export default function VanDetail() {
+    const params = useParams()
+    
     return <h1>Van detail page goes here</h1>
 }
